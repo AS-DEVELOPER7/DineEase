c6166ca039c5b83081655e99fb1d8cdc030a9efd
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2023-05-04 12:19:50 +0530

    Update TableItem.jsx

--- src/components/OrderList/TableItem.jsx
+++ src/components/OrderList/TableItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AiFillEye, AiOutlineEye } from 'react-icons/ai'
+import { AiFillEye, AiOutlineEdit, AiOutlineEye } from 'react-icons/ai'
 import { useStateContext } from '../../../context/StateContext'
 
 function TableItem({data,id}) {
@@ -38,7 +38,7 @@ function TableItem({data,id}) {
 
             <p className={`${status==='waiting'?'border-2 text-orange bg-amber-50 border-orange':status==='ready to serve'?'border-2 bg-emerald-50 text-green border-green':'border-2 text-blue bg-sky-100 border-blue'} pb-1 px-2 w-full xl:w-2/3 text-center rounded-full  font-semibold`}>{status}</p>
             </div>
-            <div className="w-1/6 flex justify-center text-blue cursor-pointer" onClick={()=>handleModal()} ><div className={`${id%2===0?'bg-white':'bg-slate-100'} p-1 rounded-md`}><AiFillEye size={20}/></div></div>
+            <div className="w-1/6 flex justify-center text-blue cursor-pointer" onClick={()=>handleModal()} ><div className={`${id%2===0?'bg-white':'bg-slate-100'} p-1 rounded-md`}><AiOutlineEdit size={20}/></div></div>
           </div>
   )
 }
